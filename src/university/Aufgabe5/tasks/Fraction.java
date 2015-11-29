05dd87e30e11ebe098f3f05757eeafebd059f3d1
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2015-11-29 15:58:15 +0100

    added aufgabe 5

--- src/university/Aufgabe5/tasks/Fraction.java
+++ src/university/Aufgabe5/tasks/Fraction.java
@@ -24,11 +24,11 @@ public class Fraction {
     // New fraction with numerator num and denominator denom; denom != 0 is required.
     public Fraction(int num, int denom) {
         if (denom < 0) { // denominator > 0 is required, but value of fraction shall be as specified by num and denom
-            num = (0 /* TODO: modify expression */);
-            denom = (0 /* TODO: modify expression */);
+            num = num * -1;
+            denom = denom * -1;
         }
-        numerator = (0 /* TODO: modify expression */);
-        denominator = (0 /* TODO: modify expression */);
+        numerator = num;
+        denominator = denom;
         reduce();
     }
 
@@ -36,21 +36,21 @@ public class Fraction {
     // If frac is null, the result shall contain a copy of this fraction (new object of same state).
     public Fraction addFrac(Fraction frac){
         if(frac == null){
-            return (null /* TODO: modify expression */);
+            return new Fraction(this.numerator, this.denominator);
         }
-        return (null /* TODO: modify expression */);
+        return new Fraction(this.numerator + frac.numerator, this.denominator + frac.denominator);
     }
 
     // Reduce the fraction. Use 0/1 for 0.
     private void reduce() {
         if (numerator == 0) {
-            denominator = (0 /* TODO: modify expression */);
+            denominator = 1;
         } else {
 
             int gcd = gcd((numerator < 0) ? (-numerator) : numerator, denominator);
 
-            numerator = (0 /* TODO: modify expression */);
-            denominator = (0 /* TODO: modify expression */);
+            numerator /= gcd;
+            denominator /= gcd;
         }
     }
 
@@ -62,19 +62,21 @@ public class Fraction {
             b = r;
             r = a % b;
         }
-        return (0 /* TODO: modify expression */);
+        return b;
     }
 
     // Adds two fractions, one specified by numerator num1 and denominator denom1, the other by numerator num1 and
     // denominator denom2. The resulting array contains the numerator at index 0 and the denominator at index 1.
     // denom1 != 0 and denom2 != 0 is required.
     public static int[] addFractions(int num1, int denom1, int num2, int denom2) {
-        return (null /* TODO: modify expression */);
+        Fraction number = new Fraction(num1 + num2, denom1 + denom2);
+        return new int[] { number.numerator, number.denominator };
     }
 
     // Just for testing ...
     public static void main(String[] args) {
         // Den Rumpf dieser Methode können Sie beliebig verändern.
+
     }
 
 }
